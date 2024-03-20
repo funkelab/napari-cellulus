347ea26ccad54a4412a02f6dc99820af0493c977
lmanan <manan.lalit@gmail.com>
2024-03-20 17:55:04 -0400

    Access samples from skin dataset

--- src/napari_cellulus/sample_data.py
+++ src/napari_cellulus/sample_data.py
@@ -1,33 +1,23 @@
 from pathlib import Path
 
 import numpy as np
-import tifffile
 
-TISSUE_NET_SAMPLE = Path(__file__).parent / "sample_data/tissue_net_sample.npy"
-FLUO_N2DL_HELA = Path(__file__).parent / "sample_data/fluo_n2dl_hela.tif"
-
-
-def fluo_n2dl_hela_sample():
-    x = tifffile.imread(FLUO_N2DL_HELA)
-    return [
-        (
-            x,
-            {
-                "name": "Raw",
-                "metadata": {"axes": ["s", "c", "y", "x"]},
-            },
-            "image",
-        )
-    ]
+TISSUE_NET_SAMPLE = (
+    Path(__file__).parent / "sample_data/tissue_net_skin_sample.npz"
+)
 
 
 def tissue_net_sample():
-    (x, y) = np.load(TISSUE_NET_SAMPLE, "r")
-    x = x.transpose(0, 3, 1, 2)
-    y = y.transpose(0, 3, 1, 2).astype(np.uint8)
+    data = np.load(TISSUE_NET_SAMPLE)
+    raw, gt = data["raw"], data["gt"]
+
+    num_samples = raw.shape[0]
+    indices = np.random.randint(0, num_samples - 1, 10)
+    raw = raw[indices]
+    gt = gt[indices]
     return [
         (
-            x,
+            raw,
             {
                 "name": "Raw",
                 "metadata": {"axes": ["s", "c", "y", "x"]},
@@ -35,7 +25,7 @@ def tissue_net_sample():
             "image",
         ),
         (
-            y,
+            gt,
             {
                 "name": "Labels",
                 "metadata": {"axes": ["s", "c", "y", "x"]},
