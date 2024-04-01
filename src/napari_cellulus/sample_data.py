a1ec96ddb273fa90c70a4f85840bdab3314be7b1
lmanan <manan.lalit@gmail.com>
2024-03-31 22:10:58 -0400

    Use npy file instead of npz file

--- src/napari_cellulus/sample_data.py
+++ src/napari_cellulus/sample_data.py
@@ -3,14 +3,12 @@ from pathlib import Path
 import numpy as np
 
 FLUO_C2DL_HUH7_SAMPLE = (
-    Path(__file__).parent / "sample_data/Fluo-C2DL-Huh7-sample.npz"
+    Path(__file__).parent / "sample_data/Fluo-C2DL-Huh7-sample.npy"
 )
 
 
 def fluo_c2dl_huh7_sample():
-    data = np.load(FLUO_C2DL_HUH7_SAMPLE)
-    raw = data["raw"]
-
+    raw = np.load(FLUO_C2DL_HUH7_SAMPLE)
     num_samples = raw.shape[0]
     indices = np.random.choice(np.arange(num_samples), 5, replace=False)
     raw = raw[indices]
